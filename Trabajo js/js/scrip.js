9a9bbeea960fdadf36ed0ca04cbfab30b39023c9
Elena Blanco Fernández <elenablancofer@gmail.com>
2022-05-18 12:53:10 +0200

    continuación trabajo

--- Trabajo js/js/scrip.js
+++ Trabajo js/js/scrip.js
@@ -1,7 +1,62 @@
-$(function(){
-    $(".calamar").draggable()
 
-    $(".cajon").droppable()
+// movimiento calamares
 
-})
 
+$(document).ready(function(){
+    animarDiv();
+    
+});
+
+function nuevaPosicionM(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    let h = $('.movimiento').height() - 300;
+    let w = $('.movimiento').width() - 200;
+    
+    let nh = Math.floor(Math.random() * h);
+    let nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animarDiv(){
+    let newq = nuevaPosicionM();
+    let oldq = $('#cmorado').offset();
+    let velocidad = calcVel([oldq.top, oldq.left], newq);
+    
+    $('#cmorado').animate({ top: newq[0], left: newq[1] }, velocidad, function(){
+        animarDiv();        
+    });
+    
+};
+
+function calcVel(prev, next) {
+    
+    let x = Math.abs(prev[1] - next[1]);
+    let y = Math.abs(prev[0] - next[0]);
+    
+    let greatest = x > y ? x : y;
+    
+    let speedModifier = 0.3;
+    
+    let velocidad = Math.ceil(greatest/speedModifier);
+    
+    return velocidad;
+    
+};
+function agarrar(){
+    if($('#cmorado').click() == true){
+        $('#cmorado').draggable() = true
+        console.log("Tocando");
+
+    }
+    else{
+        $('.movimiento') = false
+    }
+    
+    
+    
+}
+
+$(".cajon").droppable()
